a8e3cdc776d21a0cc95cf93fdb80e4a47e829a70
kamkowalczyk <kamil.kowalczyk890@gmail.com>
2022-03-19 11:53:32 +0100

    done_IEnumerable

--- lab3_pudelko/lab3_pudelko/Pudelko.cs
+++ lab3_pudelko/lab3_pudelko/Pudelko.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,52 +8,50 @@ using System.Threading.Tasks;
 
 namespace lab3_pudelko
 {
-    public sealed class Pudelko : IEquatable<Pudelko>
+    public sealed class Pudelko : IEquatable<Pudelko>, IEnumerable<decimal>
     {
         // przechowuje w metrach
         private decimal _a;
         private decimal _b;
         private decimal _c;
+        private decimal[] _parameters;
 
         public decimal A
         {
-            get { return Math.Round(_a, 3); }
-            set { _a = value; }
+            get => Math.Round(_a, 3);
+            init => _a = value;
         }
 
         public decimal B
         {
-            get { return Math.Round(_b, 3); }
-            set { _b = value; }
+            get => Math.Round(_b, 3);
+            init => _b = value;
         }
 
         public decimal C
         {
-            get { return Math.Round(_c, 3); }
-            set { _c = value; }
+            get => Math.Round(_c, 3);
+            init => _c = value;
         }
 
-        public UnitOfMeasure UnitOfMeasure { get; set; }
+        public UnitOfMeasure UnitOfMeasure { get; init; }
 
-        public decimal Objetosc
-        {
-            get { return Math.Round(_a * _b * _c, 9); }
-        }
-        public decimal Pole
-        {
-            get { return Math.Round(_a * _b * 2 + _a * _c * 2 + _b * _c * 2, 6); }
-        }
+        public decimal Objetosc => Math.Round(_a * _b * _c, 9);
+        public decimal Pole => Math.Round(_a * _b * 2 + _a * _c * 2 + _b * _c * 2, 6);
 
-        public Pudelko(decimal a = 0.1m, decimal b = 0.1m, decimal c = 0.1m, UnitOfMeasure unitOfMeasure = UnitOfMeasure.Meter)
+        public Pudelko(decimal a = 0.1m, decimal b = 0.1m, decimal c = 0.1m,
+            UnitOfMeasure unitOfMeasure = UnitOfMeasure.Meter)
         {
             if (a < 0 || b < 0 || c < 0 || a > 10 || b > 10 || c > 10)
             {
                 throw new ArgumentOutOfRangeException();
             }
+
             UnitOfMeasure = unitOfMeasure;
             A = MeasureConverter.ConvertToMeters(a, UnitOfMeasure);
             B = MeasureConverter.ConvertToMeters(b, UnitOfMeasure);
             C = MeasureConverter.ConvertToMeters(c, UnitOfMeasure);
+            _parameters = new[] { A, B, C };
         }
 
         public override bool Equals(object? obj)
@@ -61,9 +60,8 @@ namespace lab3_pudelko
             if (ReferenceEquals(this, obj)) return true;
             if (!(obj is Pudelko)) return false;
             var box = (Pudelko)obj;
-            var parameters = new List<decimal> { A, B, C };
-            var parametersCompared = new List<decimal> { box.A, box.B, box.C };
-            return parameters.All(parametersCompared.Contains) && parameters.Count == parametersCompared.Count;
+            var parametersCompared = new[] { box.A, box.B, box.C };
+            return _parameters.All(parametersCompared.Contains) && _parameters.Length == parametersCompared.Length;
         }
 
         public bool Equals(Pudelko? box)
@@ -75,29 +73,66 @@ namespace lab3_pudelko
         {
             return HashCode.Combine(A, B, C);
         }
+
         public static bool operator ==(Pudelko? leftBox, Pudelko? rightBox)
         {
             if (leftBox is null && rightBox is null)
             {
                 return true;
             }
+
             if (leftBox is null && rightBox is not null)
             {
                 return false;
             }
+
             if (leftBox is not null && rightBox is null)
             {
                 return false;
             }
+
             return leftBox.Equals(rightBox);
         }
 
         public static bool operator !=(Pudelko? leftBox, Pudelko? rightBox) => !(leftBox == rightBox);
 
+        public static Pudelko operator +(Pudelko leftBox, Pudelko rightBox)
+        {
+            var leftBoxParameters = new[] { leftBox.A, leftBox.B, leftBox.C }.OrderByDescending(a => a).ToArray();
+            var rightBoxParameters = new[] { rightBox.A, rightBox.B, rightBox.C }.OrderByDescending(a => a).ToArray();
+            var a = new[] { leftBoxParameters[0], rightBoxParameters[0] }.Max();
+            var b = new[] { leftBoxParameters[1], rightBoxParameters[1] }.Max();
+            var c = leftBoxParameters[2] + rightBoxParameters[2];
+            return new Pudelko(a, b, c);
+            // sprobuj przy testach z _parameters
+        }
+
+        public static explicit operator double[](Pudelko box) => new[]
+            { Convert.ToDouble(box.A), Convert.ToDouble(box.B), Convert.ToDouble(box.C) };
+
+        public static implicit operator Pudelko(ValueTuple<int, int, int> values) =>
+            new(values.Item1, values.Item2, values.Item3, UnitOfMeasure.Milimeter);
+
+        public decimal this[int index] => _parameters[index];
+
         public override string ToString()
         {
             return $"{A} m × {B} m × {C} m";
         }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<decimal> GetEnumerator()
+        {
+            foreach (var parameter in _parameters)
+            {
+                yield return parameter;
+            }
+        }
+
         public string ToString(string format)
         {
             return format switch
